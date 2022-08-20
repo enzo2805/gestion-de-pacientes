8a7fc25479fdcd493dfc59d119f596af495a7942
Enzo Maximiliano Rodriguez <enzo2805@gmail.com>
2022-08-20 18:23:47 -0300

    first commit

---
+++ php/src/db.php
@@ -0,0 +1,19 @@
+<?php
+//These are the defined authentication environment in the db service
+
+// The MySQL service named in the docker-compose.yml.
+$host = 'db';
+
+// Database use name
+$user = 'MYSQL_USER';
+
+//database user password
+$pass = 'MYSQL_PASSWORD';
+
+// database name
+$mydatabase = 'MYSQL_DATABASE';
+// check the mysql connection status
+
+$conn = new mysqli($host, $user, $pass, $mydatabase);
+
+?>
